63603d69d5b9431d248181e58f4b6424a2798d42
Ashkan <a.farhadi@pishro.computer>
2021-01-02 17:01:40 +0330

    Added two more examples

---
+++ src/2_indentation.py
@@ -0,0 +1,19 @@
+"""
+every signle scope of a python code
+starts with space.
+how many space? as much the developer desires, but at least one space is required.
+**it's better to use one-tab or 2/4 spaces .
+"""
+
+
+# For example, this piece of code is OKAY
+if True:
+    print("Oops!")
+
+# BUT this one will raise an error
+
+"""
+if True:
+print("Oops!")
+
+"""
---
+++ src/3_variables.py
@@ -0,0 +1,36 @@
+"""
+Python is dynamically typed language ( and it's awesome :D )
+It means thay you don't have the specify the type of your variables.
+** Python is case-sensitive
+Let's see the example:
+
+"""
+
+# These are python built-in data types, we will focus on each of them in next examples.
+
+# String
+str_var = "I am a string."
+
+# Boolean
+bool_var = True  # or False
+
+# Integer
+int_var = 13
+
+# Float
+float_var = 9.34
+
+# Complex number
+complex_var = 1 + 2j
+
+# List
+list_var = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_var = (1, 2, 3, 4, 5)
+
+# Dictionary
+dict_var = {"name": "Ashkan", "age": 26}
+
+# set
+set_var = {"Python", "GoLang", "C++"}
